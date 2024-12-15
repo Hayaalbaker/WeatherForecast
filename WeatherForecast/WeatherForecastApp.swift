5b1cb15a48cee2111d66f901714c2749b1a09d7b
Hayaalbaker <144670605+Hayaalbaker@users.noreply.github.com>
2024-12-15 13:47:39 +0300

    Initial Commit

---
+++ WeatherForecast/WeatherForecastApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherForecastApp.swift
+//  WeatherForecast
+//
+//  Created by Haya Albaker on 15/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherForecastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
